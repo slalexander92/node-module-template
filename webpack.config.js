125de631eb3cdee4353ec240812ac067e07dfeef
Tommyduong23 <tommyduong23@gmail.com>
2018-09-10 11:45:26 -0700

    change output file to index

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require( 'path' );
 module.exports = {
   entry: path.resolve( __dirname, 'src/main.js' ),
   output: {
-    filename: '[MODULE_NAME].js',
+    filename: 'index.js',
 		path: path.resolve( __dirname, 'dist' ),
 		library: '[MODULE_NAME]',
 		libraryTarget: 'umd',
